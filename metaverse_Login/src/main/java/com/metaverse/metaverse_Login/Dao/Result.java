a4443da52bbf282b0701d47032e1355173939373
Hainui <1273393857@qq.com>
2024-09-02 17:56:33 +0800

    修改成泛型

--- metaverse_Login/src/main/java/com/metaverse/metaverse_Login/Dao/Result.java
+++ metaverse_Login/src/main/java/com/metaverse/metaverse_Login/Dao/Result.java
@@ -7,22 +7,26 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Result {
-    //开发规范
-    private Integer code;//响应码，1 代表成功; 0 代表失败
-    private String msg;  //响应信息 描述字符串
-    private Object data; //返回的数据
+public class Result<T> {
+    // 响应码，1 代表成功; 0 代表失败
+    private Integer code;
+    // 响应信息 描述字符串
+    private String msg;
+    // 返回的数据
+    private T data;
 
-    //增删改 成功响应
-    public static Result success(){
-        return new Result(1,"success",null);
+    // 增删改 成功响应
+    public static <T> Result<T> success() {
+        return new Result<>(1, "success", null);
     }
-    //查询 成功响应
-    public static Result success(Object data){
-        return new Result(1,"success",data);
+
+    // 查询 成功响应
+    public static <T> Result<T> success(T data) {
+        return new Result<>(1, "success", data);
     }
-    //失败响应
-    public static Result error(String msg){
-        return new Result(0,msg,null);
+
+    // 失败响应
+    public static <T> Result<T> error(String msg) {
+        return new Result<>(0, msg, null);
     }
 }
