60766705a1cefa53885830a87e2139efff9c54f8
Hainui <79104833+Hainui@users.noreply.github.com>
2024-09-02 17:34:43 +0800

    Add files via upload

---
+++ metaverse_Login/src/main/java/com/metaverse/metaverse_Login/MetaverseLoginApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.metaverse_Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaverseLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaverseLoginApplication.class, args);
+	}
+
+}
