78c292e6e4c27e05761bdfafe8e471baa168a0ea
Hainui <1273393857@qq.com>
2024-09-03 11:54:41 +0800

    dd

--- src/main/java/com/metaverse/metaverse_Login/Controller/LoginController.java
+++ src/main/java/com/metaverse/metaverse_Login/Controller/LoginController.java
@@ -1,9 +1,6 @@
 package com.metaverse.metaverse_Login.Controller;
 
-import com.metaverse.metaverse_Login.Dao.Result;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
