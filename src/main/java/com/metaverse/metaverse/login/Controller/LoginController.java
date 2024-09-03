3044c74caa63193fca035d090b71db84e29a8930
Hainui <1273393857@qq.com>
2024-09-03 11:56:20 +0800

    dd

---
+++ src/main/java/com/metaverse/metaverse/login/Controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.metaverse.metaverse.login.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LoginController {
+
+}
---
+++ src/main/java/com/metaverse/metaverse/login/exception/GlobalException.java
@@ -0,0 +1,17 @@
+package com.metaverse.metaverse.login.exception;
+
+
+import com.metaverse.metaverse.login.Dao.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+//全局异常处理器
+public class GlobalException {
+
+    @ExceptionHandler(Exception.class)//指定捕获的异常类型
+    public Result ex (Exception ex){
+        ex.printStackTrace();
+        return Result.error("操作失败,请联系管理员");
+    }
+}
