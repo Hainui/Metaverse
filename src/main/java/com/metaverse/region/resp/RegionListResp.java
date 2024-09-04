fb46079157b8ec7c21899be1d50fa9ab4d74c354
Hainui <1273393857@qq.com>
2024-09-04 14:19:26 +0800

    mybatis-plus

--- src/main/java/com/metaverse/region/resp/RegionListResp.java
+++ src/main/java/com/metaverse/region/resp/RegionListResp.java
@@ -1,9 +1,13 @@
 package com.metaverse.region.resp;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegionListResp {
 
     @ApiModelProperty(value = "区服id", required = true)
