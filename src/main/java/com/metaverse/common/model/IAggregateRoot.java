e0abe26caa9f7504bedcb7c0e31e78eff4ca103a
Hainui <1273393857@qq.com>
2024-09-05 20:31:00 +0800

    update

--- src/main/java/com/metaverse/common/model/IAggregateRoot.java
+++ src/main/java/com/metaverse/common/model/IAggregateRoot.java
@@ -1,9 +1,9 @@
 package com.metaverse.common.model;
 
 /**
- * 聚合根
+ * 聚合根：领域对象，具有自己的行为，比如人类是一个聚合根，人类可以更改自己的用户名是这个人类具备的行为
  *
- * @param <T>
+ * @param <T> 对应的贫血模型
  */
 public interface IAggregateRoot<T> {
 }
