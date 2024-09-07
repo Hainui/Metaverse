8e8fd00e82ba5fc5020e6bdf2af2899dc85f62ff
Hainui <1273393857@qq.com>
2024-09-07 18:38:15 +0800

    update

--- src/main/java/com/metaverse/common/model/IEntity.java
+++ src/main/java/com/metaverse/common/model/IEntity.java
@@ -1,9 +1,11 @@
 package com.metaverse.common.model;
 
+import java.io.Serializable;
+
 /**
  * 实体：可以离开聚合根存活 - 比如人的背包装备
  */
-public interface IEntity {
+public interface IEntity extends Serializable {
     Long pkVal();
 
     Long modelVersion();
--- src/main/java/com/metaverse/common/model/IValueObject.java
+++ src/main/java/com/metaverse/common/model/IValueObject.java
@@ -1,9 +1,11 @@
 package com.metaverse.common.model;
 
+import java.io.Serializable;
+
 /**
  * 值对象：必须依附聚合根才能存活 - 比如人类身上的器官
  */
-public interface IValueObject {
+public interface IValueObject extends Serializable {
 
     Long modelVersion();
 }
