3bbecada9e075196af93e537a255c2e69ea0a9d8
xiaoze2023 <315626261@qq.com>
2024-09-21 20:29:04 +0800

    xiaoze: update

---
+++ src/main/java/com/metaverse/common/Utils/AliOSSUtils.java
@@ -0,0 +1,47 @@
+package com.metaverse.common.Utils;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyuncs.exceptions.ClientException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+/**
+ * 阿里云 OSS 工具类
+ */
+@Component
+public class AliOSSUtils {
+
+
+    /**
+     * 实现上传图片到OSS
+     */
+    public String upload(MultipartFile file) throws IOException, ClientException {
+
+        // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+
+        String endpoint = "https://oss-cn-fuzhou.aliyuncs.com";
+        String bucketName = "xiaoze-hmleadnews";
+
+        InputStream inputStream = file.getInputStream();
+        String originalFilename = file.getOriginalFilename();
+        String fileName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+
+
+        //上传文件到 OSS
+        OSS ossClient = new OSSClientBuilder().build(endpoint, credentialsProvider);
+        ossClient.putObject(bucketName, fileName, inputStream);
+        //文件访问路径
+        String url = endpoint.split("//")[0] + "//" + bucketName + "." + endpoint.split("//")[1] + "/" + fileName;
+        ossClient.shutdown();
+        return url;
+    }
+
+}
