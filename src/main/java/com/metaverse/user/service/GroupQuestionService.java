8fe3a7aaef2f7a2a5f0ac13917488ac331178589
Hainui <1273393857@qq.com>
2024-09-30 13:01:53 +0800

    Hainui Hainui Hainui Hainui Hainui Hainui Hainui Hainui

--- src/main/java/com/metaverse/user/service/GroupQuestionService.java
+++ src/main/java/com/metaverse/user/service/GroupQuestionService.java
@@ -7,6 +7,7 @@ import com.metaverse.user.req.AnswerGroupQuestionReq;
 import com.metaverse.user.req.GroupReq;
 import com.metaverse.user.resp.UserGroupQuestionResp;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,7 +18,8 @@ import java.util.Objects;
 @Service
 @RequiredArgsConstructor
 public class GroupQuestionService {
-    private final GroupJoinRequestService groupJoinRequestService;
+    @Setter
+    private GroupJoinRequestService groupJoinRequestService;
     private final IMetaverseGroupQuestionService groupQuestionService;
 
     @Transactional(rollbackFor = Exception.class)
